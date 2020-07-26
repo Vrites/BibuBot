31e973302ae7a78ce68bf2dcc990ae96e38026d1
Ville Riepponen <villeriepponen@gmail.com>
2020-07-26 19:59:29 +0300

    Event creator
    Hard coded event creator, will make it better in the future

--- index.js
+++ index.js
@@ -250,5 +250,49 @@ client.on('message', (message) => {
     }
 });
 
+client.on('message', (message) => {
+    if(message.content == '!create' && message.member.id == '100267154837872640') {
+        const msg = message.channel.send('Glory mountit\nTapahtuu: 30.7.2020\nKello: 18:00\nLeader: <@164054723345907712>\n')
+        .then(async function(message){
+            await message.react('736990100663304232');
+            await message.react('736990085609816084');
+            await message.react('736990051149545472');
+            const tankFilter = (reaction, user) => {
+                return reaction.emoji.id === '736990100663304232' && user.id != client.user;
+            };
+            const healFilter = (reaction, user) => {
+                return reaction.emoji.id === '736990085609816084' && user.id != client.user;
+            };
+            const dpsFilter = (reaction, user) => {
+                return reaction.emoji.id === '736990051149545472' && user.id != client.user;
+            };
+            const cancelFilter = (reaction, user) => {
+                return reaction.emoji.id === '736990068413300786' && user.id == '100267154837872640';
+            };
+
+            const tankCollector = message.createReactionCollector(tankFilter, { max: 2, time: 345600000 });
+            const healCollector = message.createReactionCollector(healFilter, { time: 345600000 });
+            const dpsCollector = message.createReactionCollector(dpsFilter, { time: 345600000 });
+            const cancelCollector = message.createReactionCollector(cancelFilter, { time: 345600000 });
+
+            tankCollector.on('collect', (reaction, user) => {
+                message.edit(message.content + '\n<:tank:736990100663304232>' + '<@' + user + '>\n');
+            });
+            healCollector.on('collect', (reaction, user) => {
+                message.edit(message.content + '\n<:heal:736990085609816084>' + '<@' + user + '>\n');
+            });
+            dpsCollector.on('collect', (reaction, user) => {
+                message.edit(message.content + '\n<:dps:736990051149545472>' + '<@' + user + '>\n');
+            });
+            cancelCollector.on('collect', (reaction, user) => {
+                message.delete();
+            });
+
+        
+        });
+
+    }
+});
+
 
 client.login(process.env.token);

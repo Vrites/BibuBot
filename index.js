17651e12d4213343697224ea7b99933a0a610126
Ville Riepponen <villeriepponen@gmail.com>
2020-08-05 10:08:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.Help + '```');
-        })
+        });
     }
 });
 
@@ -50,8 +50,8 @@ client.on('message', (message) => {
                 return;
             }
             const vendorData = JSON.parse(jsonString);
-            message.channel.send('```' + vendorData.rotationFive + '```');
-        })
+            message.channel.send('```' + vendorData.rotationSix + '```');
+        });
     }
 });
 
@@ -63,8 +63,8 @@ client.on('message', (message) => {
                 return;
             }
             const vendorData = JSON.parse(jsonString);
-            message.channel.send('```' + vendorData.rotationSix + '```');
-        })
+            message.channel.send('```' + vendorData.rotationSeven + '```');
+        });
     }
 });
 
@@ -76,8 +76,8 @@ client.on('message', (message) => {
                 return;
             }
             const vendorData = JSON.parse(jsonString);
-            message.channel.send('```' + vendorData.rotationSeven + '```');
-        })
+            message.channel.send('```' + vendorData.rotationEight + '```');
+        });
     }
 });
 
@@ -89,8 +89,8 @@ client.on('message', (message) => {
                 return;
             }
             const vendorData = JSON.parse(jsonString);
-            message.channel.send('```' + vendorData.rotationEight + '```');
-        })
+            message.channel.send('```' + vendorData.rotationOne + '```');
+        });
     }
 });
 
@@ -103,7 +103,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.Severe + '```');
-        })
+        });
     }
 });
 
@@ -116,7 +116,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.IS + '```');
-        })
+        });
     }
 });
 
@@ -129,7 +129,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.Masterful + '```');
-        })
+        });
     }
 });
 
@@ -142,7 +142,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.Ineffable + '```');
-        })
+        });
     }
 });
 
@@ -155,7 +155,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.TD + '```');
-        })
+        });
     }
 });
 
@@ -168,7 +168,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.Versatile + '```');
-        })
+        });
     }
 });
 
@@ -181,7 +181,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.Expedient + '```');
-        })
+        });
     }
 });
 
@@ -194,7 +194,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.MindFlay + '```');
-        })
+        });
     }
 });
 
@@ -207,7 +207,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.VoidRitual + '```');
-        })
+        });
     }
 });
 
@@ -220,7 +220,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.GW + '```');
-        })
+        });
     }
 });
 
@@ -233,7 +233,7 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.Strikethrough + '```');
-        })
+        });
     }
 });
 
@@ -246,63 +246,63 @@ client.on('message', (message) => {
             }
             const vendorData = JSON.parse(jsonString);
             message.channel.send('```' + vendorData.Avoidant + '```');
-        })
+        });
     }
 });
 
-client.on('message', (message) => {
-    if(message.content == '!create' && message.member.id == '100267154837872640') {
-        const msg = message.channel.send('Glory mountit\nTapahtuu: 30.7.2020\nKello: 18:00\nLeader: <@164054723345907712>\n')
-        .then(async function(message){
-            await message.react('736990100663304232');
-            await message.react('736990085609816084');
-            await message.react('736990051149545472');
-            await message.react('736990068413300786');
-            const tankFilter = (reaction, user) => {
-                return reaction.emoji.id === '736990100663304232' && user.id != client.user;
-            };
-            const healFilter = (reaction, user) => {
-                return reaction.emoji.id === '736990085609816084' && user.id != client.user;
-            };
-            const dpsFilter = (reaction, user) => {
-                return reaction.emoji.id === '736990051149545472' && user.id != client.user;
-            };
-            const cancelFilter = (reaction, user) => {
-                return reaction.emoji.id === '736990068413300786' && user.id == '100267154837872640';
-            };
+// client.on('message', (message) => {
+//     if(message.content == '!create' && message.member.id == '100267154837872640') {
+//         const msg = message.channel.send('Glory mountit\nTapahtuu: 30.7.2020\nKello: 18:00\nLeader: <@164054723345907712>\n')
+//         .then(async function(message){
+//             await message.react('736990100663304232');
+//             await message.react('736990085609816084');
+//             await message.react('736990051149545472');
+//             await message.react('736990068413300786');
+//             const tankFilter = (reaction, user) => {
+//                 return reaction.emoji.id === '736990100663304232' && user.id != client.user;
+//             };
+//             const healFilter = (reaction, user) => {
+//                 return reaction.emoji.id === '736990085609816084' && user.id != client.user;
+//             };
+//             const dpsFilter = (reaction, user) => {
+//                 return reaction.emoji.id === '736990051149545472' && user.id != client.user;
+//             };
+//             const cancelFilter = (reaction, user) => {
+//                 return reaction.emoji.id === '736990068413300786' && user.id == '100267154837872640';
+//             };
 
-            const tankCollector = message.createReactionCollector(tankFilter, { max: 2, time: 345600000 });
-            const healCollector = message.createReactionCollector(healFilter, { time: 345600000 });
-            const dpsCollector = message.createReactionCollector(dpsFilter, { time: 345600000 });
-            const cancelCollector = message.createReactionCollector(cancelFilter, { time: 345600000 });
+//             const tankCollector = message.createReactionCollector(tankFilter, { max: 2, time: 345600000 });
+//             const healCollector = message.createReactionCollector(healFilter, { time: 345600000 });
+//             const dpsCollector = message.createReactionCollector(dpsFilter, { time: 345600000 });
+//             const cancelCollector = message.createReactionCollector(cancelFilter, { time: 345600000 });
 
-            tankCollector.on('collect', (reaction, user) => {
-                if(!message.content.includes(user)){
-                    message.edit(message.content + '\n<:tank:736990100663304232>' + '<@' + user + '>\n');
-                }
-            });
-            healCollector.on('collect', (reaction, user) => {
-                if(!message.content.includes(user)){
-                    message.edit(message.content + '\n<:heal:736990085609816084>' + '<@' + user + '>\n');
-                }
-            });
-            dpsCollector.on('collect', (reaction, user) => {
-                if(!message.content.includes(user)){
-                    message.edit(message.content + '\n<:dps:736990051149545472>' + '<@' + user + '>\n');
-                }
-            });
-            cancelCollector.on('collect', (reaction, user) => {
-                if(user == '100267154837872640'){
-                    message.delete();
-                }
+//             tankCollector.on('collect', (reaction, user) => {
+//                 if(!message.content.includes(user)){
+//                     message.edit(message.content + '\n<:tank:736990100663304232>' + '<@' + user + '>\n');
+//                 }
+//             });
+//             healCollector.on('collect', (reaction, user) => {
+//                 if(!message.content.includes(user)){
+//                     message.edit(message.content + '\n<:heal:736990085609816084>' + '<@' + user + '>\n');
+//                 }
+//             });
+//             dpsCollector.on('collect', (reaction, user) => {
+//                 if(!message.content.includes(user)){
+//                     message.edit(message.content + '\n<:dps:736990051149545472>' + '<@' + user + '>\n');
+//                 }
+//             });
+//             cancelCollector.on('collect', (reaction, user) => {
+//                 if(user == '100267154837872640'){
+//                     message.delete();
+//                 }
 
-            });
+//             });
 
         
-        });
+//         });
 
-    }
-});
+//     }
+// });
 
 
 client.login(process.env.token);
